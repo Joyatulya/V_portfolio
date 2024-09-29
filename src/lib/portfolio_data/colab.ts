aebb3e61daede907ca4d2e2f1c28117eb09d4482
Joy <joy.atulya@gmail.com>
2024-09-29 10:11:54 +0100

    added hindi fonts

--- src/lib/portfolio_data/colab.ts
+++
@@ -1,9 +0,0 @@
-type notebook = {
-	name : string
-	url : string
-	description : string
-}
-
-export const COLAB_NOTEBOOKS  : notebook[] = [
-
-]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,12 @@
 <script lang="ts">
-	import AcademicComponent from './AcademicComponent.svelte';
-	import GithubRepoComponent from './GithubRepoComponent.svelte';
+	const name_hindi = 'वैभवी ';
+	const name_english = 'Vaibhavi';
+	let name = $state(name_hindi);
+
 </script>
 
 <main>
-	<div class="h-screen">
-		<h1 class="text-9xl font-bold">
-			Hero
-		</h1>
-	</div>
-	<AcademicComponent />
-	<GithubRepoComponent />
+	<section class="flex h-screen items-center justify-center">
+		<h1 class="text-[10em] font-bold">{name}</h1>
+	</section>
 </main>
-
-<style>
-	#orb {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		width: 150px;
-		height: 150px;
-		background: radial-gradient(
-			circle,
-			rgba(255, 255, 255, 1) 0%,
-			rgba(0, 123, 255, 1) 60%,
-			rgba(0, 123, 255, 0.7) 100%
-		);
-		border-radius: 50%;
-		transform: translate(-50%, -50%);
-		animation: float 5s ease-in-out infinite alternate;
-		box-shadow: 0px 0px 100px 10px rgba(0, 123, 255, 0.6);
-		z-index: -10;
-		filter: blur(60px);
-	}
-	/* Float Animation */
-@keyframes float {
-    0% {
-        transform: translate(-50vw, -50vh) translateY(0);
-    }
-    50% {
-        transform: translate(-50%, -50%) translateY(-30px);
-    }
-    100% {
-        transform: translate(+30vw, +30vh) translateY(0);
-    }
-}
-</style>
